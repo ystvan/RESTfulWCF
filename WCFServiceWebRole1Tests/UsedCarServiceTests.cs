4af5bab3c124ca426d68274f6280d91907910b5b
Istvan Marki <istv0050@edu.easj.dk>
2016-12-23 17:09:00 +0100

    UT

---
+++ WCFServiceWebRole1Tests/UsedCarServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFServiceWebRole1;
+using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServiceWebRole1.Tests
+{
+    [TestClass()]
+    public class UsedCarServiceTests
+    {
+
+        private IUsedCarService service;
+        private Car car1, car2, car3;
+
+        [TestInitialize]
+        public void StartTest()
+        {
+            service = new UsedCarService();
+        }
+
+        [TestMethod]
+        public void Implementation()
+        {
+            Assert.IsInstanceOfType(service, typeof(IUsedCarService));
+        }
+
+
+        [TestMethod()]
+        public void GetAllCarsTest()
+        {
+            IList<Car> testCars = service.GetAllCars();
+            Assert.IsNotNull(testCars);
+            Assert.AreEqual(testCars.Count, 4);
+        }
+
+       
+    }
+}
